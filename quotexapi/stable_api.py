6378a7408d8ec50adfb3f90856b3340803e1fcd5
avinashzaware2025 <avinashzaware2025@gmail.com>
2025-04-22 15:18:50 +0530

    Create stable_api.py

---
+++ quotexapi/stable_api.py
@@ -0,0 +1,8 @@
+class Quotex:
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+    def login(self):
+        print("✅ Login Successful")
+        return True
